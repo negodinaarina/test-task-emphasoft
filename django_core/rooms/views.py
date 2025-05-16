a942a0c40a6eac45b33dde3afaf30f2f1ad1f904
negodinaarina <negodinaarina24@gmail.com>
2025-05-16 14:01:36 +0300

    chore: remove redundant function

--- django_core/rooms/views.py
+++ django_core/rooms/views.py
@@ -19,27 +19,6 @@ from rooms.serializers import (
     RoomRetrieveSerializer,
 )
 
-
-def make_room_filters(params):
-    filters = {}
-
-    min_price = params.get("min_price")
-    max_price = params.get("max_price")
-    min_capacity = params.get("min_capacity")
-    max_capacity = params.get("max_capacity")
-
-    if min_price is not None:
-        filters["price_per_day__gte"] = min_price
-    if max_price is not None:
-        filters["price_per_day__lte"] = max_price
-    if min_capacity is not None:
-        filters["capacity__gte"] = min_capacity
-    if max_capacity is not None:
-        filters["capacity__lte"] = max_capacity
-
-    return filters
-
-
 @extend_schema_view(
     list=extend_schema(
         tags=["Rooms"],
